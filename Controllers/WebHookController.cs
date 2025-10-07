53c4633cd282e68a0ddd0caa3a6d1d1303b5f487
RustamKholov <roshfor22@gmail.com>
2025-10-07 13:48:42 +0200

    Add initial project structure with WebHookController and configuration for Telegram Bot

---
+++ Controllers/WebHookController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ReflectaBot.Controllers;
+
+[ApiController]
+[Route("api/update")]
+public class WebHookController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly TelegramBotClient _botClient;
+
+    public WebHookController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _botClient = new TelegramBotClient((Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN") ?? configuration["Telegram:BotToken"]) ?? string.Empty);;
+        
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Update update)
+    {
+        if (update.Message?.Chat.Id != null)
+        {
+            var chatId = update.Message.Chat.Id;
+            var messageText = update.Message.Text;
+            await _botClient.SendMessage(
+                chatId: chatId,
+                text: $"Hello from controller, your text: {messageText}");
+        }
+        return Ok();
+    }
+    
+}
